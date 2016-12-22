9d5e98ac7a016d80bc42c6c34c5dd0ac479d62e5
Tom Milburn <thomas.milburn@rutgers.edu>
2016-12-22 13:20:30 -0500

    simple admin interface test

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
+var env = require('env2')('./.env');
+
+console.log(process.env.PHOTO_PASS);
 
 var app = express();
 app.set('view engine', 'pug');
@@ -11,12 +14,18 @@ var Photo = mongoose.model('Photo', {filename: String, title: String});
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', function(req, res){
-	res.render("simpleview.pug", {array: ["uh", 2, 3, 4, 5]});
+	res.render("simple.pug", {photos: ["00001.jpg", "00002.jpg", 3, 4, 5]});
 });
 
-app.post('/', function(req, res){
+app.get('/admin', function(req, res){
+	res.render("admin.pug", {array: ["uh", 2, 3, 4, 5]});
+});
+app.post('/admin', function(req, res){
   console.log("post request");
-	console.log(req.body)
+	if(req.body.password === process.env.PHOTO_PASS){
+		console.log('correct password');
+		console.log(req.body)
+	};
 });
 
 app.listen(4001);
