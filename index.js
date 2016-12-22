a6d0a39b25cb6a125979066333b342aa1920fa72
Tom Milburn <thomas.milburn@rutgers.edu>
2016-12-22 13:03:22 -0500

    basic post request test

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 var express = require('express');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 
 var app = express();
+app.set('view engine', 'pug');
+
+mongoose.connect('mongodb://localhost/photos');
+var Photo = mongoose.model('Photo', {filename: String, title: String});
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', function(req, res){
-	res.send("hello world");
+	res.render("simpleview.pug", {array: ["uh", 2, 3, 4, 5]});
+});
+
+app.post('/', function(req, res){
+  console.log("post request");
+	console.log(req.body)
 });
 
 app.listen(4001);
